47a6259dc062ed2ff8d22e36d6b60a058f6eac57
juliette magin <juliette.magin@gmail.com>
2023-08-24 16:08:24 +0200

    added form :'(

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,9 +1,29 @@
 class BirdsController < ApplicationController
-  
   def index
     @birds = Bird.all
   end
+
   def show
     @bird = Bird.find(params[:id])
+  end
+
+  def new
+    @bird = Bird.new
+  end
+
+  def create
+    @bird = Bird.new(bird_params)
+    @bird.birdlord_id = current_user.id
+    if @bird.save
+      redirect_to bird_path(@bird)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bird_params
+    params.require(:bird).permit(:name, :photo, :species, :details, :price_per_day)
   end
 end
