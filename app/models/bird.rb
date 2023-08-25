ab3f2e43c38fbf7867fac506e8c223c103c744b6
Tatiana Parfait <tania.parfait@hotmail.fr>
2023-08-25 10:44:10 +0200

    wishlist pas okay

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -4,4 +4,8 @@ class Bird < ApplicationRecord
   has_one_attached :photo
   validates :name, :details, :species, :price_per_day, :birdlord_id, presence: true
   # validates :photo, uniqueness: true
+
+  # POUR LA WISHLIST
+  has_many :wishes
+  has_many :wishlisted_by_users, through: :wishes, source: :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_24_200513) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_25_073104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -83,9 +83,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_24_200513) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wishes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "bird_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bird_id"], name: "index_wishes_on_bird_id"
+    t.index ["user_id"], name: "index_wishes_on_user_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "birds", "users", column: "birdlord_id"
   add_foreign_key "bookings", "birds"
   add_foreign_key "bookings", "users", column: "renter_id"
+  add_foreign_key "wishes", "birds"
+  add_foreign_key "wishes", "users"
 end
